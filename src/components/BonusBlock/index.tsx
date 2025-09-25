e1c2eb128a876a1c084d63402f26ce1b5184b39b
Vitaliy <v.maslyonik@easyrocket.net>
2025-09-25 11:40:00 +0300

    [FE-1]: add

--- src/components/BonusBlock/index.tsx
+++ src/components/BonusBlock/index.tsx
@@ -2,7 +2,7 @@ import freespinAmount from 'src/shared/assets/200.webp';
 
 export const BonusBlock = () => {
   return (
-    <div className="relative left-0 top-0 z-[3] flex h-auto flex-col items-center justify-center contain-layout contain-paint 768:left-12 768:top-48">
+    <div className="relative left-0 top-0 z-[3] flex h-auto flex-col items-center justify-center contain-layout contain-paint 768:left-12 768:top-[20vh]">
       <div className="mt-[35px] flex h-[170px] w-[420px] items-center justify-center bg-FLAME_BG bg-cover bg-center bg-no-repeat pl-1.5 768:h-[340px] 768:w-[820px]">
         <img
           className="absolute top-[27px] w-[280px] object-contain object-center 768:w-[545px]"
@@ -13,7 +13,7 @@ export const BonusBlock = () => {
           <p className="text-[39px] font-black text-azure 768:text-[78px]">ФРИСПИНОВ</p>
         </div>
       </div>
-      <p className="leading-0 -translate-y-2 font-MTS_Sans text-[25px] font-black text-white [text-shadow:0_4px_0_#000] 768:text-[50px]">
+      <p className="leading-0 -translate-y-7 font-MTS_Sans text-[25px] font-black text-white [text-shadow:0_4px_0_#000] 768:text-[50px]">
         ЗА РЕГИСТРАЦИЮ
       </p>
     </div>
--- src/components/ImagesBlock/index.tsx
+++ src/components/ImagesBlock/index.tsx
@@ -47,7 +47,7 @@ export const ImagesBlock: FC = () => {
         loading="lazy"
         src={azure}
         alt="azure"
-        className="absolute left-0 top-[32%] z-[1] w-[230px] 768:w-[560px]"
+        className="absolute left-0 top-[32%] z-[1] h-[650px] w-[230px] 768:w-[560px]"
       />
       <img
         loading="lazy"
